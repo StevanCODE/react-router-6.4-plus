343d7a6c2ad769c473d6506a95a7b554c42bc239
Stevan Popovic <pop.stevan@gmail.com>
2023-07-06 12:08:11 +0200

    login added and data fetching refactored

--- index.jsx
+++ index.jsx
@@ -4,12 +4,12 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans, { loader as vansLoader } from "./pages/Vans/Vans";
-import VanDetail from "./pages/Vans/VanDetail";
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
-import HostVans from "./pages/Host/HostVans";
-import HostVanDetail from "./pages/Host/HostVanDetail";
+import HostVans, { loader as hostVansLoader } from "./pages/Host/HostVans";
+import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
@@ -19,21 +19,23 @@ import HostLayout from "./components/HostLayout";
 
 import "./server";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
       <Route path="vans" element={<Vans />} loader={vansLoader} errorElement={<ErrorPage />} />
-      <Route path="vans/:id" element={<VanDetail />} />
+      <Route path="vans/:id" element={<VanDetail />} loader={vanDetailLoader} />
 
       <Route path="host" element={<HostLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="income" element={<Income />} />
         <Route path="reviews" element={<Reviews />} />
-        <Route path="vans" element={<HostVans />} />
-        <Route path="vans/:id" element={<HostVanDetail />}>
+        <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
+        <Route path="vans/:id" element={<HostVanDetail />} loader={hostVanDetailLoader}>
           <Route index element={<HostVanInfo />} />
           <Route path="pricing" element={<HostVanPricing />} />
           <Route path="photos" element={<HostVanPhotos />} />
--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,46 +1,31 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+
+export const loader = async ({ params }) => {
+  const response = await fetch("/api/host/vans/");
+  const data = await response.json();
+  return data.vans;
+};
 
 export default function HostVans() {
-    const [vans, setVans] = React.useState([])
+  const vans = useLoaderData();
 
-    React.useEffect(() => {
-        fetch("/api/host/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
+  const hostVansEls = vans.map((van) => (
+    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ));
 
-    const hostVansEls = vans.map(van => (
-        <Link
-            to={van.id}
-            key={van.id}
-            className="host-van-link-wrapper"
-        >
-            <div className="host-van-single" key={van.id}>
-                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
-                </div>
-            </div>
-        </Link>
-    ))
-
-    return (
-        <section>
-            <h1 className="host-vans-title">Your listed vans</h1>
-            <div className="host-vans-list">
-                {
-                    vans.length > 0 ? (
-                        <section>
-                            {hostVansEls}
-                        </section>
-
-                    ) : (
-                            <h2>Loading...</h2>
-                        )
-                }
-            </div>
-        </section>
-    )
+  return (
+    <section>
+      <h1 className="host-vans-title">Your listed vans</h1>
+      <div className="host-vans-list">{vans.length > 0 ? <section>{hostVansEls}</section> : <h2>Loading...</h2>}</div>
+    </section>
+  );
 }
